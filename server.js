83e89c380d6bab02a99e239dfbd6eadb385f96f0
chiheb <chihababiza9@gmail.com>
2024-06-23 15:22:19 +0100

    Complete weather fetching functionality and update view

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-// server.js
 const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+// Load environment variables from the .env file
+dotenv.config();
+
 const app = express();
 
 // Set the view engine to EJS
@@ -8,12 +13,32 @@ app.set("view engine", "ejs");
 // Serve the public folder as static files
 app.use(express.static("public"));
 
-// server.js
+// Render the index template with default values for weather and error
 app.get("/", (req, res) => {
     res.render("index", { weather: null, error: null });
 });
 
+// Handle the /weather route
+app.get("/weather", async (req, res) => {
+    const city = req.query.city;
+    const apiKey = process.env.API_KEY; // Use the API key from environment variables
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    let weather;
+    let error = null;
+    try {
+        const response = await axios.get(APIUrl);
+        weather = response.data;
+    } catch (err) {
+        weather = null;
+        error = "Error, Please try again";
+    }
+    res.render("index", { weather, error });
+});
+
+// Start the server and listen on port 3000 or the value of the PORT environment variable
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
 });
