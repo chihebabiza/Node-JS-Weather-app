e4b613b991b29daca26fb68188527031ac3f4ae9
chiheb <chihababiza9@gmail.com>
2024-06-23 15:19:43 +0100

    Set up basic server with Express

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require("express");
+const app = express();
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+
+// Serve the public folder as static files
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
